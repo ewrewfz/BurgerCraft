95468b0371f420df0876e84d5d0f4e822a140db8
Jemin <bryun97@naver.com>
2025-09-22 00:07:35 +0900

    Update,Add) 폴더 계층 구조 정리 & 싱글톤,유틸 스크립트 추가

---
+++ Assets/@Scripts/Manager/Singleton.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+   private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            // 처음 사용할 때만
+            if (_instance == null)
+            {
+                _instance = (T)GameObject.FindAnyObjectByType(typeof(T));
+
+                if (_instance != null)
+                {
+                    GameObject go = new GameObject(); // typeof(T).Name, typeof(T)
+                    T t = go.AddComponent<T>();
+                    t.name = $"@{typeof(T).Name}";
+
+                    _instance = t;
+                }
+                DontDestroyOnLoad(_instance.gameObject);
+
+            }
+            return _instance;
+        }
+    }
+}
