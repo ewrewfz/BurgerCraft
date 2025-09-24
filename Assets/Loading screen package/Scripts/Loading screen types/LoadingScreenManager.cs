d53a4e039168eaf7c02d173edf0e2b95dfbb6135
jmlee <bryun97@naver.com>
2025-09-24 16:45:01 +0900

    Add) BackEnd 서버 구축 & 로딩 씬 ThirdParty 추가

---
+++ Assets/Loading screen package/Scripts/Loading screen types/LoadingScreenManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreenManager : MonoBehaviour
+{
+    private Animator _animatorComponent;
+
+    private void Start()
+    {
+        _animatorComponent = transform.GetComponent<Animator>();  
+
+        // Remove it if you don't want to hide it in the Start function and call it elsewhere
+        HideLoadingScreen();
+    }
+
+    public void RevealLoadingScreen()
+    {
+        _animatorComponent.SetTrigger("Reveal");
+    }
+
+    public void HideLoadingScreen()
+    {
+        // Call this function, if you want start hiding the loading screen
+        _animatorComponent.SetTrigger("Hide");
+    }
+
+    public void OnFinishedReveal()
+    {
+        OnFinishedHide();
+
+    }
+
+    public void OnFinishedHide()
+    {
+        SceneManager.LoadScene("DevScene");
+    }
+
+}
