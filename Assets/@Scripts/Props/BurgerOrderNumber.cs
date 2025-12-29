edc2925f97c60e82fcb2cd4916300940abaa29b5
jmlee <bryun97@naver.com>
2025-12-29 10:48:28 +0900

    Remove) TestCode 삭제

--- Assets/@Scripts/Props/BurgerOrderNumber.cs
+++ Assets/@Scripts/Props/BurgerOrderNumber.cs
@@ -1,5 +1,4 @@
 using UnityEngine;
-using TMPro;
 
 /// <summary>
 /// 버거에 주문 번호를 저장하는 컴포넌트
@@ -7,9 +6,6 @@ using TMPro;
 public class BurgerOrderNumber : MonoBehaviour
 {
     [SerializeField] private string _orderNumber;
-    [SerializeField] private TextMeshProUGUI _orderNumberText; // 인스펙터에서 직접 할당
-    private Transform _canvasTransform;
-    private Camera _mainCamera;
     
     public string OrderNumber 
     { 
@@ -17,55 +13,6 @@ public class BurgerOrderNumber : MonoBehaviour
         set 
         { 
             _orderNumber = value;
-            UpdateOrderNumberText();
-        }
-    }
-    
-    private void Awake()
-    {
-        // Canvas 찾기
-        Canvas canvas = Utils.FindChild<Canvas>(gameObject);
-        if (canvas != null)
-        {
-            _canvasTransform = canvas.transform;
-        }
-        
-        // 메인 카메라 찾기
-        _mainCamera = Camera.main;
-        if (_mainCamera == null)
-        {
-            _mainCamera = FindObjectOfType<Camera>();
-        }
-    }
-    
-    private void OnEnable()
-    {
-        // 활성화될 때 주문번호 텍스트 업데이트
-        UpdateOrderNumberText();
-    }
-    
-    private void LateUpdate()
-    {
-        // Canvas가 항상 카메라를 바라보도록 회전
-        if (_canvasTransform != null && _mainCamera != null)
-        {
-            Vector3 directionToCamera = _mainCamera.transform.position - _canvasTransform.position;
-            if (directionToCamera != Vector3.zero)
-            {
-                _canvasTransform.rotation = Quaternion.LookRotation(directionToCamera) * Quaternion.Euler(0, 180, 0);
-            }
-        }
-    }
-    
-    /// <summary>
-    /// 주문번호 텍스트를 UI에 표시합니다.
-    /// </summary>
-    private void UpdateOrderNumberText()
-    {
-        if (_orderNumberText != null)
-        {
-            // 주문번호 : (GUID) 형식으로 표시
-            _orderNumberText.text = !string.IsNullOrEmpty(_orderNumber) ? $"주문번호 : {_orderNumber}" : "주문번호 :";
         }
     }
     
